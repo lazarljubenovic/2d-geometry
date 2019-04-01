0922b158b9b950aa24916913b3c423bab6f77ac1
lazarljubenovic <lazar.ljubenovic.1995@gmail.com>
2019-04-01 12:03:28 +0200

    fix: export Angle from Geometry

--- src/geometry/index.ts
+++ src/geometry/index.ts
@@ -2,10 +2,12 @@ import * as Point from './point'
 import * as Aabb from './aabb'
 import * as VectorIntersection from './vector-intersection'
 import * as Intersection from './intersection'
+import * as Angle from './angle'
 
 export {
   Point,
   Aabb,
   VectorIntersection,
   Intersection,
+  Angle,
 }
